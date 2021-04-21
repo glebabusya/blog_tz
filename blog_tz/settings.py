2f8e3be228c5fad0ca51ec1495b460277092c44c
glebabusya <glebabusya@mail.ru>
2021-04-22 01:38:29 +0300

    deploy

--- blog_tz/settings.py
+++ blog_tz/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get('DEBUG', default=0))
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
