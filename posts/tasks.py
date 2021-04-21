a4a9b6172ced34c20db5e4a15460644f3fede002
glebabusya <glebabusya@mail.ru>
2021-04-22 02:34:31 +0300

    deploy

--- posts/tasks.py
+++ posts/tasks.py
@@ -1,9 +1,12 @@
 import logging
+
+from celery import shared_task
+
 from blog_tz.celery import app
 from posts.models import Upvote
 
 
-@app.task
+@shared_task
 def reset_upvotes():
     Upvote.objects.all().delete()
-    return 'oaksdajsdapsdj'
+    return 'Upvotes Deleted'
