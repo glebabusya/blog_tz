0e45f103cff3a343bb69763589ffc0ac3113c0a7
glebabusya <glebabusya@mail.ru>
2021-04-22 21:58:40 +0300

    settings

--- blog_tz/settings.py
+++ blog_tz/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", default="foo")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get('DEBUG', 0))
+DEBUG = int(os.environ.get('DEBUG', 1))
 
 ALLOWED_HOSTS = ["*"]
 
