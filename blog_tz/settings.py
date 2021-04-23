b5ba871770416b38acd59d02c8dca5a04746cd8c
glebabusya <glebabusya@mail.ru>
2021-04-23 15:13:00 +0300

    staticfiles

--- blog_tz/settings.py
+++ blog_tz/settings.py
@@ -134,7 +134,6 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static'),
     os.path.join(BASE_DIR, 'build')
 ]
 
