625e9bf212e2c578272f527682655544376c5eda
glebabusya <glebabusya@mail.ru>
2021-04-23 11:13:48 +0300

    settings

--- blog_tz/settings.py
+++ blog_tz/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default="foo")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 # Application definition
 
