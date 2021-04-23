b9dc954baad92501130469f366d69ffca8116987
glebabusya <glebabusya@mail.ru>
2021-04-23 15:27:06 +0300

    staticfiles

--- blog_tz/urls.py
+++ blog_tz/urls.py
@@ -17,12 +17,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("posts/", include("posts.urls")),
     path("comments/", include("comments.urls")),
 ]
 
 urlpatterns += [path("api-auth/", include("rest_framework.urls"))]
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
