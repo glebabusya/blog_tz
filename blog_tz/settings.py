236b3bb2405c25d0551b07c6ed8df6f5dbe15ea0
glebabusya <glebabusya@mail.ru>
2021-04-23 14:54:25 +0300

    settings

--- blog_tz/settings.py
+++ blog_tz/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         "PORT": os.environ.get("DB_PORT", ""),
     }
 }
-print(os.environ)
+
 if "DATABASE_URL" in os.environ:
     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
