6a5849380fba43925ab9e22500c6cb856a27d298
glebabusya <glebabusya@mail.ru>
2021-04-23 15:15:39 +0300

    staticfiles

--- blog_tz/settings.py
+++ blog_tz/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
